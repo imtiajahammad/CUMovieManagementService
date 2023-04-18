5121cefb899c75128057186d9a2e3643519ba49b
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-04-18 17:07:20 +0600

    Added Genre Entity

--- MovieManagement.Domain/Entities/Genre.cs
+++ MovieManagement.Domain/Entities/Genre.cs
@@ -3,7 +3,9 @@ namespace MovieManagement.Domain.Entities
 {
 	public class Genre
 	{
-
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public List<Movie>? Movies { get; set; }
 	}
 }
 
