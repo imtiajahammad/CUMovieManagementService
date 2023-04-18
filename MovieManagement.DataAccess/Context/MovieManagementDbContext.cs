bfb7b844a34b02c44057f0e39a0a8d4f8383b0ab
imtiajahammad <imtiaj.pcMac@outlook.com>
2023-04-18 17:09:17 +0600

    Added DbContext, DbSets, add domain project ref

---
+++ MovieManagement.DataAccess/Context/MovieManagementDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MovieManagement.Domain.Entities;
+
+namespace MovieManagement.DataAccess.Context
+{
+	public class MovieManagementDbContext : DbContext
+	{
+		public MovieManagementDbContext(DbContextOptions<MovieManagementDbContext> options) : base(options) { }
+
+		public DbSet<Actor> Actors { get; set; }
+		public DbSet<Biography> Biographies { get; set; }
+		public DbSet<Genre> Genres { get; set; }
+		public DbSet<Movie> Movies { get; set; }
+	}
+}
+
